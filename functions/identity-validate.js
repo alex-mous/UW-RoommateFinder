33cb63d7873738759aa9646ceaadb18c1114dfb0
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-03-29 03:17:43 -0700

    Add test function

--- functions/identity-validate.js
+++ functions/identity-validate.js
@@ -1,7 +1,9 @@
 exports.handler = async (ev, ctx) => {
     console.log(ev);
+    const {identity, user} = ctx.clientContext;
+    console.log(identity, user);
     return {
-        statusCode: 400,
+        statusCode: 200,
         body: JSON.stringify({"app_metadata": {"roles": ["admin"]}})
     }
 }
