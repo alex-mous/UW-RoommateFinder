e00cb2f6596750509bd8f6e2b701c34fab7ee851
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-03-31 14:44:03 +0400

    Create profile.js

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -19,9 +19,10 @@ window.onload = () => {
 
     document.querySelector("textarea[name='bio']").onkeyup = (e) => {
         document.querySelector("#bioCount").innerText = e.srcElement.value.split(" ").length;
-        var elem = document.getElementById("bioCount");
-        elem.classList.toggle("text-center small text-success", e.srcElement.value.split(" ").length<150);
-        elem.classList.toggle("text-center small text-danger", e.srcElement.value.split(" ").length>=150); 
+        let elem = document.getElementById("bioCount");
+        let len = e.srcElement.value.split(" ").length
+        elem.classList.toggle("text-center small text-success", len<150);
+        elem.classList.toggle("text-center small text-danger", len>=150); 
         if (e.srcElement.value.split(" ").length >= 150 && e.which >= 0x20) {
             e.preventDefault();
         }
@@ -73,6 +74,7 @@ const doLogout = () => {
 
 const doUpdate = (e) => {
     e.preventDefault();
+    console.log(e.target);
     let data = new FormData(e.target);
     /*
     Ranking system:
@@ -109,7 +111,6 @@ const doUpdate = (e) => {
                 lgbtq: data.get("lgbtqpref"),
             //Ternary or more
                 pronouns: data.get("pronouns"),
-                
 
             },
             prefsRanked: { //Preferences that are preferrable if they exists, but not like flags and are not subtractable like Minimized ones
@@ -117,15 +118,14 @@ const doUpdate = (e) => {
                 state: data.get("state"), //May be null
                 major: data.get("major"),
                 sport: data.get("sport"),
-                interests: data.getAll("interests"),
+                clubs: data.getAll("interests"),
                 ideology: {
                     rank: data.get("ideologyr"),
-                    al: data.get("ideology1"),
-                    lr: data.get("ideology2")
+                    lr: data.get("ideology1"),
+                    al: data.get("ideology2")
                 },
                 location: data.get("campus"),
-                hall: data.get("residence"),
-                lgbtq: data.get("lgbtq"),
+                hall: data.get("hall")
             },
             prefsMinimized: { //Preferences that can be minimized by subtraction - all numerical values
                 cleanliness: data.get("cleanliness"),
@@ -138,103 +138,20 @@ const doUpdate = (e) => {
                 temperature: data.get("temperature"),
                 waketime: parseInt(data.get("waketime"))*60 + parseInt(data.get("waketime").slice(-2)), //Minutes
                 sleeptime: parseInt(data.get("sleeptime"))*60 + parseInt(data.get("sleeptime").slice(-2)) //Minutes
-            },
+            }
         }
     }
-    showMsg("Updating your profile...", "resMsg", "info")
     auth.currentUser().update({
         data: {
             ...user
         }
-    }).then((u) => {
-        console.log("User now: ", u);
-        showMsg("Successfully updated your profile!", "resMsg", "success");
-    });
+    }).then(u => console.log(u));
 
 }
 
 //Load all of the form data from user memory (precondition - user is logged in)
 const loadForm = () => {
-    let user = auth.currentUser().user_metadata;
-    document.querySelector("input[name='name']").value = user.listing.name;
-    document.querySelector("input[name='email']").value = user.listing.email;
-    document.querySelector("textarea[name='bio']").value = user.listing.bio;
-    document.querySelector("#bioCount").value = user.listing.bio.split(" ").length;
-
-    if (user.profile.prefsRanked.country != null) {
-        let yesBx = document.querySelector("input[name='countrymatch'][value='y']");
-        yesBx.checked = true;
-        document.querySelector("input[name='countrymatch']").onchange({
-            target: yesBx
-        });
-        let countrySelect = document.querySelector("select[name='country']");
-        countrySelect.value = user.profile.prefsRanked.country;
-        if (user.profile.prefsRanked.state) {
-            let stateSelect = document.querySelector("select[name='state']");
-            stateSelect.value = user.profile.prefsRanked.state;
-            countrySelect.onchange({
-                target: countrySelect
-            });
-        }
-    } else {
-        document.querySelector("input[name='countrymatch'][value='n']").checked = true;
-    }
-
-    document.querySelector(`input[name='pronouns'][value='${user.profile.prefsAbs.pronouns}']`).checked = true;
-    document.querySelector(`input[name='social'][value='${user.profile.prefsMinimized.social}']`).checked = true;
-
-    document.querySelector("select[name='closeness']").value = user.profile.prefsMinimized.closeness;
-    document.querySelector("select[name='major']").value = user.profile.prefsRanked.major;
-    document.querySelector("select[name='campus']").value = user.profile.prefsRanked.location;
-    document.querySelector("select[name='residence']").value = user.profile.prefsRanked.hall;
-    document.querySelector("select[name='temperature']").value = user.profile.prefsMinimized.temperature;
-    document.querySelector("select[name='cleanliness']").value = user.profile.prefsMinimized.cleanliness;
-    document.querySelector("select[name='noise']").value = user.profile.prefsMinimized.noise;
-    document.querySelector("select[name='pplover']").value = user.profile.prefsMinimized.pplover;
-    document.querySelector("select[name='visover']").value = user.profile.prefsMinimized.visover;
-    document.querySelector("select[name='sport']").value = user.profile.prefsRanked.sport;
-    document.querySelector("select[name='ideology1']").value = user.profile.prefsRanked.ideology.al;
-    document.querySelector("select[name='ideology2']").value = user.profile.prefsRanked.ideology.lr;
-
-    document.querySelector(`input[name='rushing'][value='${user.profile.prefsMinimized.rushing}']`).checked = true;
-    document.querySelector(`input[name='drink'][value='${user.profile.prefsAbs.drink.me}']`).checked = true;
-    document.querySelector(`input[name='drinkr'][value='${user.profile.prefsAbs.drink.you}']`).checked = true;
-    document.querySelector(`input[name='vape'][value='${user.profile.prefsAbs.vape.me}']`).checked = true;
-    document.querySelector(`input[name='vaper'][value='${user.profile.prefsAbs.vape.you}']`).checked = true;
-    document.querySelector(`input[name='smoke'][value='${user.profile.prefsAbs.smoke.me}']`).checked = true;
-    document.querySelector(`input[name='smoker'][value='${user.profile.prefsAbs.smoke.you}']`).checked = true;
-    document.querySelector(`input[name='weed'][value='${user.profile.prefsAbs.weed.me}']`).checked = true;
-    document.querySelector(`input[name='weedr'][value='${user.profile.prefsAbs.weed.you}']`).checked = true;
-
-    for (let opt of document.querySelector("select[name='interests']").options) {
-        if (user.profile.prefsRanked.interests.includes(opt.value)) opt.selected = true;
-        else opt.selected = false;
-    }
-
-    let lgbtqRad = document.querySelector(`input[name='lgbtq'][value='${user.profile.prefsRanked.lgbtq}']`);
-    lgbtqRad.checked = true;
-    document.querySelector("input[name='lgbtq']").onchange({
-            target: lgbtqRad
-    });
-    if (user.profile.prefsRanked.lgbtq == "y") document.querySelector(`input[name='lgbtqpref'][value='${user.profile.prefsAbs.lgbtq}']`).checked = true;
-
-    let ideologyRad = document.querySelector(`input[name='ideologyr'][value='${user.profile.prefsRanked.ideology.rank}']`);
-    ideologyRad.checked = true;
-    document.querySelector("input[name='ideologyr']").onchange({
-            target: ideologyRad
-    });
-
-    let setTime = (name, time) => {
-        let hrs = Math.floor(time/60);
-        if (hrs < 10) hrs = `0${hrs}`;
-        let mins = time%60;
-        if (mins < 10) mins = `0${mins}`; //Pad in case not correct format
-        document.querySelector(`input[name='${name}']`).value = `${hrs}:${mins}`;
-    }
-
-    setTime("waketime", user.profile.prefsMinimized.waketime);
-    setTime("sleeptime", user.profile.prefsMinimized.sleeptime);
-
+    
 }
 
 //Show or clear a message. htmlMsg store the html to be displayed. blockId is the ID of the element to target. type is the text type (info/success/warn/danger)
