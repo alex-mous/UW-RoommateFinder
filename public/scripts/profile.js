6e99e8e6b48b55439d68425efdf59538a75ef4e6
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-03-31 12:45:22 +0400

    Update profile.js

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -20,10 +20,10 @@ window.onload = () => {
     document.querySelector("textarea[name='bio']").onkeyup = (e) => {
         document.querySelector("#bioCount").innerText = e.srcElement.value.split(" ").length;
         var elem = document.getElementById("bioCount");
+        elem.classList.toggle("text-center small text-success", e.srcElement.value.split(" ").length <150);
+        elem.classList.toggle("text-center small text-danger", e.srcElement.value.split(" ").length >=150); 
         if (e.srcElement.value.split(" ").length >= 150 && e.which >= 0x20) {
             e.preventDefault();
-            elem.classList.remove("text-center small text-success");
-            elem.classList.add("text-center small text-danger");
         }
     }
 
