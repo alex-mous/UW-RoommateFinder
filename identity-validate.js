0745cb55be1bfd55454b35d0a03112cc490337ca
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-04-02 15:34:44 -0700

    Restructure project further

--- identity-validate.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (ev, ctx) => {
-    const evt = JSON.parse(ev.body);
-    const user = evt.user;
-    console.log("User: ", user);
-    console.log("Has UW email: ", user.email.endsWith("@uw.edu"));
-    return {
-        statusCode: user.email.endsWith("@uw.edu") ? 200 : 400
-    }
-}
