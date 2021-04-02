9d2c2eb4c43b21cd965e843f54ea562891d40791
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-04-01 23:24:38 -0700

    Add start of match algorithm

--- functions/match.js
+++ functions/match.js
@@ -1,11 +1,39 @@
 import fetch from "node-fetch";
 
 exports.handler = (ev, ct, cb) => {
-    const data = JSON.parse(ev.body);
-    console.log("EV", ev);
-    console.log("CT", ct);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({success: true, test: ct.clientContext})
+    const {identity, user} = ct.clientContext;
+    const data = ev.body;
+    console.log("ID", identity);
+    console.log("User", user);
+    console.log("Data", data);
+
+    if (!identity) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({success: true, test: ct.clientContext})
+        }
     }
+
+    return fetch(`${identity.url}/admin/users`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${identity.token}`
+        }
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            console.log("Results:", res);
+            return {
+                statusCode: 200,
+                body: JSON.stringify({success: true, body: res})
+            }
+        })
+        .catch((err) => {
+            console.error("Failed to fetch users:", err);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({success: false, body: err})
+            }
+        })
+
 }
--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -7,5 +7,24 @@ window.onload = () => {
 
     document.querySelector("#logoutBtn").onclick = doLogout;
 
-    if (!user.user_metadata) showMsg("Please go to <a href='/profile'>Profile</a> to complete your profile and start getting matches")
+    if (!user.user_metadata) showMsg("Please go to <a href='/profile'>Profile</a> to complete your profile and start getting matches");
+
+    user.jwt(true).then((token) => {
+        fetch("/.netlify/functions/match",
+        {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            credentials: "include",
+            body: JSON.stringify(
+                {test:"abc"}
+            )
+        })
+            .then(res => console.log(res));//;res.json())
+            //.then(res => console.log(res))
+    })
+
+    
 }
