381c1f599a65f52fb89573631ef5ead575af1fe8
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-04-02 01:24:04 -0700

    attempt to fix bug

--- functions/match.js
+++ functions/match.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+/*import fetch from "node-fetch";
 
 exports.handler = async (ev, ct) => {
     try {
@@ -41,4 +41,30 @@ exports.handler = async (ev, ct) => {
             body: JSON.stringify({success: false, err: err})
         }
     }
-}
+}*/
+
+import fetch from 'node-fetch';
+
+exports.handler = async (event, context) => {
+    const { identity, user } = context.clientContext;
+    const usersUrl = `${identity.url}/admin/users`;
+    const adminAuthHeader = `Bearer ${identity.token}`;
+
+    try {
+        return fetch(usersUrl, {
+            method: 'GET',
+            headers: { Authorization: adminAuthHeader },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Resulting data", JSON.stringify(data));
+                return { statusCode: 204 };
+            })
+            .catch((error) => ({
+                statusCode: 500,
+                body: `Internal Server Fault: ${error}`,
+            }));
+    } catch (error) {
+        return error;
+    }
+};
