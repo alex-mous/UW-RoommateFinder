2d6e40d043d5010edede5e562dd10c38d64787b9
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2021-04-04 17:52:30 -0700

    Fix bugs with support form and continue with ranking system

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
     if (user == null) { //Exit page if not logged in
-        window.location.href = "/"
+        window.location.href = "/";
     }
 
     document.querySelector("#logoutBtn").onclick = doLogout;
@@ -12,20 +12,11 @@ window.onload = () => {
         return;
     }
 
-    console.log("Fetching matches...");
-    /*fetch("/.netlify/functions/runmatch", {
-        headers: {
-            'Authorization': 'Bearer ' + user.token.access_token
-        },
-        credentials: 'include'
-    })
-        .then(res => res.json())
-        .then(res => {*/
-    console.log("Results: ", res);
+    console.log("Showing matches...");
     if (user.app_metadata.success) {
-        for (let user of user.app_metadata.users) {
+        for (let usr of user.app_metadata.users) {
             let row = document.createElement("TR");
-            row.innerHTML = `<td>${user.name}</td><td>${user.email}</td><td><div class="bio">${user.bio}</div></td><td>${user.match}</td>`;
+            row.innerHTML = `<td>${usr.name}</td><td>${usr.email}</td><td><div class="bio">${usr.bio}</div></td><td>${usr.match}</td>`;
             document.querySelector("#matchTable").appendChild(row);
         }
     } else {
