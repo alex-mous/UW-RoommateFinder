924ab47764864f7c18e95d8cb742738f0e1b9059
Nahush Shrivatsa <67535204+nahushvatsa@users.noreply.github.com>
2021-04-06 12:52:34 +0400

    success message change

--- public/scripts/support.js
+++ public/scripts/support.js
@@ -8,6 +8,6 @@ window.onload = () => {
 	document.querySelector("#logoutBtn").onclick = doLogout;
 
 	if (window.location.hash.includes("success")) {
-		showMsg("Successfully sent message! We will get back to you soon.", "resMsg", "success");
+		showMsg("Response successfully sent! We will get back to you soon.", "resMsg", "success");
 	}
 }
